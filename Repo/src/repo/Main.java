1deff125451656ff7070a9e20c80d514fbaa8296
Kalen <friesenka@wcmp15vs005lt.blads.ca>
2025-09-24 09:54:24 -0500

    Delete everything

--- Repo/src/repo/Main.java
+++ Repo/src/repo/Main.java
@@ -3,13 +3,7 @@ package repo;
 public class Main {
 
 	public static void main(String[] args) {
-		// added line comment - Kalen
-		/*
-		 * Added multiline comment
-		 */
-		for (int i=0; i<3; i++) {
-			System.out.println(i+1);
-		}
+		
 	}
 
 }
