1205f6b7094234b6b2d42c3f013775067bf279b4
Vito <stoeszvi@stu.blsd.ca>
2025-09-24 09:30:14 -0500

    Added a multiline comment

--- Repo/src/repo/Main.java
+++ Repo/src/repo/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// added line comment - Kalen
+		/*
+		 * Added multiline comment
+		 */
 		for (int i=0; i<3; i++) {
 			System.out.println(i+1);
 		}
